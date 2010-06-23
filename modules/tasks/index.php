3a44829c1e1c07a11767366d40045d8eeddf6ab7
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-06-23 16:28:51 +0300

    Added login page for user login verification

--- modules/tasks/index.php
+++ modules/tasks/index.php
@@ -1,80 +1,45 @@
+<!--
+        index.html
+        
+        Copyright 2010 John Francis Mukulu <john.f.mukulu@gmail.com>
+        
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+        
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+        
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+        MA 02110-1301, USA.
+-->
 <?php
 	include('config.php');
-	$page_tag = $_GET['page'];
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Intranet System for ITIDO</title>
-<link rel="stylesheet" type="text/css" href="/intranet/modules/tasks/stylesheets/main.css" media="screen" />
+	<title>Login Form</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.18" />
 </head>
+
 <body>
-<div id="header">
-	<div id="menu">
-		<ul>
-			<li class="current"><a href="#">Events</a></li>
-			<li><a href="#">News</a></li>
-			<li><a href="#">Gallery</a></li>
-			<li><a href="#">Projects</a></li>
-			<li><a href="#">Training</a></li>
-			<li><a href="#">Remainders</a></li>
-			<li><a href="#">Polls</a></li>
-			<li><a href="#">Chat</a></li>
-		</ul>
-	</div>
-	<!-- end #menu -->
-	<div id="logo">
-		<h1><a href="#">Intranet</a></h1>
-		<p>Internal Network services.</p>
-	</div>
-	<!-- end #logo -->
-</div>
-<!-- end #header -->
-<?php submenu($page_tag) ?>
-<div id="page">
-	<div id="content">
-	<h2>Contents stays here!</h2>
-	<h3><?php echo $url_dashboard . "    " . $url_profile ?></h3>
-<!--
-		<div class="post">
-			<h2 class="title">Welcome to our website </h2>
-			<p class="byline"><small>Posted by FreeCssTemplates</small></p>
-			<div class="entry">
-				<p><strong>Cellulose </strong> is a free template from <a href="http://www.freecsstemplates.org/">Free CSS Templates</a> released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution</a>. You're free to use this template for both commercial or personal use. I only ask that you link back to <a href="http://www.freecsstemplates.org/">my site</a> in some way. Enjoy :)</p>
-			</div>
-			<div class="meta">
-				<p class="links"><a href="#" class="comments">Comments (32)</a> &nbsp;&bull;&nbsp;&nbsp; <a href="#" class="more">Read full post &raquo;</a></p>
-			</div>
-		</div>
-		<div class="post">
-			<h2 class="title">Lorem Ipsum Dolor Volutpat</h2>
-			<p class="byline"><small>Posted by FreeCssTemplates</small></p>
-			<div class="entry">
-				<p>Curabitur tellus. Phasellus tellus turpis, iaculis in, faucibus lobortis, posuere in, lorem. Donec a ante. Donec neque purus, adipiscing id, eleifend a, cursus vel, odio. Vivamus varius justo sit amet leo. Morbi sed libero. Vestibulum blandit augue at mi. Praesent fermentum lectus eget diam. Nam cursus, orci sit amet porttitor iaculis, ipsum massa aliquet nulla, non elementum mi elit a mauris.</p>
-				<ul>
-					<li><a href="#">Magna lacus bibendum mauris</a></li>
-					<li><a href="#">Velit semper nisi molestie</a></li>
-					<li><a href="#">Magna lacus bibendum mauris</a></li>
-					<li><a href="#">Velit semper nisi molestie</a></li>
-				</ul>
-			</div>
-			<div class="meta">
-				<p class="links"><a href="#" class="comments">Comments (32)</a> &nbsp;&bull;&nbsp;&nbsp; <a href="#" class="more">Read full post &raquo;</a></p>
-			</div>
-		</div>
--->
-	</div>
-	<!-- end #content -->
-	<?php sidebar($page_tag) ?>
-	<!-- end #sidebar -->
-	<div style="clear: both;">&nbsp;</div>
-</div>
-<!-- end #page -->
-<div id="footer">
-	<p>Copyright &copy; 2010. All Rights Reserved. Design by John Francis Mukulu.</p>
-</div>
-<!-- end #footer -->
+	<form action="<?php echo $verificationPage ?>" method="post" class="form">
+		<h5><?php echo $_GET['error'] ?></h5>
+		<fieldset>
+			<legend>Login Form</legend>
+			<p><label for="username">Username</label> <input name="username" id="input" type="text"></p>
+			<p><label for="password">Password</label> <input name="password" id="input" type="password"></p>
+			<p class="submit"><input value="Login" type="submit"></p>
+		</fieldset>
+	</form>
 </body>
 </html>
