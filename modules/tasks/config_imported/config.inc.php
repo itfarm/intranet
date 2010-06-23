91b266b82a40ba30b6b807e2f1270622ea2983a6
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-06-24 00:00:50 +0300

    Imported config.inc that sets couple of global functions and for enabling/disabling debug mode

---
+++ modules/tasks/config_imported/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once('settings.inc.php');
+
+// from include file
+Global $szRootPath, $szRootURL, $szSection;
+
+Global $blutcDebug,$intDefaultVariation;
+
+// set this to true if you need to debug this module
+$blutcDebug = false;
+
+// This is the Name of the Navigation section that the Admin belongs to
+$szSection = 'Users & Groups';
+
+?>
