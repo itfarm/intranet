b0f033001e14022c8b1fee421ec95eb192889327
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-06-26 00:41:48 +0300

    Added change password content script for changing user's password in personal account

---
+++ modules/tasks/profile/changepassword.php
@@ -0,0 +1,69 @@
+<?php 
+	include_once('../config.php');
+	# include the header
+	global $szSection, $szSubSection, $szTitle, $szRootPath, $intDisplayLanguage;
+	$szSection = 'Personal Tools';
+	$szSubSection = 'Change Password';
+	$szTitle = 'Change Password';
+	include('auth.php');
+	
+	$user = new pmo_auth();
+	
+	if ($_POST['change'] && $_POST['change']=="Change Password"){
+		$arrUserInfo = $user->changepwd($_POST);
+	}
+	
+
+?>
+<form name="changepwd" method="post" action="changepwd.php">
+<input type="hidden" name="id" value="<?=$_SESSION['userInfo']['id']?>">
+<input type="hidden" name="username" value="<?=$_SESSION['userInfo']['uname']?>">
+<table class="adminTable" width="100%" border="0" cellspacing="1" cellpadding="4" align="center">
+        <tr> 
+          <td colspan="2" class="adminHeader">Change Password</td>
+        </tr>
+		
+   <!--	 message		-->
+  <? if (isset($arrUserInfo['message'])){?>
+  <tr valign="middle"> 
+          <td class="adminRow2 smalltext header3" colspan="2" valign=top align="center"><?=$arrUserInfo['message']?></td>
+  </tr>
+  <? } ?>
+    <!-- 	username	-->
+  
+  <tr valign="middle"> 
+          <td class="adminRow1 smalltext" valign=top>Username</td>
+          <td class="adminRow1 smalltext header3"><?=$_SESSION['userInfo']['uname']?><br></td>
+        </tr>
+
+  
+  <!-- 	oldpassword	-->
+  
+  <tr valign="middle"> 
+          <td class="adminRow2 smalltext" valign=top>Old Password*</td>
+          <td class="adminRow2 smalltext"><input class=vform type="password" name="oldpasswd" size="20" maxlength="30" value="<?=$oldpasswd?>"><br></td>
+        </tr>
+  
+  <!--	newpassword		-->
+  
+  <tr valign="middle"> 
+          <td class="adminRow1 smalltext" valign=top>New Password*</td>
+          <td class="adminRow1 smalltext"><input class=vform type="password" name="newpasswd" size="20" maxlength="30" value="<?=$newpasswd?>"><br></td>
+        </tr>
+		
+ <!--	newpassword		-->
+  
+  <tr valign="middle"> 
+          <td class="adminRow2 smalltext" valign=top>Confirm New Password*</td>
+          <td class="adminRow2 smalltext"><input class=vform type="password" name="confirmpwd" size="20" maxlength="30" value="<?=$confirmpwd?>"><br></td>
+        </tr>
+		
+  <!--	change button		-->
+  
+  <tr valign="middle"> 
+          <td class="authTableFooter" valign=top>&nbsp;</td>
+          <td class="authTableFooter"><input type="submit" name="change" value="Change Password" class="button" onClick="return checkChangepwdForm()"><br></td>
+        </tr>
+
+	</table>
+</form>
