67c3dcb4a1624dbaa6dbec2a7dfc1b668669f49c
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-06-27 21:55:39 +0300

    Upgrade view teams page

--- modules/tasks/dashboard/viewteams.php
+++ modules/tasks/dashboard/viewteams.php
@@ -7,11 +7,13 @@
 	@include_once('config_imported/functions.inc.php');
 	// initialize host and database
     @include_once('config_imported/settings.inc.php');
+    $search_string = $_POST['search_string'];
     
 ?>
 
-<form name="filter_form" method="Get" action="<?php echo $homePage ?>">
-	<input type="text" name="page" value="view_users" style="display:none" />	
+<form name="filter_form" method="POST" action="<?php echo $homePage ?>">
+	<input type="text" name="page" value="dashboard" style="display:none" />
+	<input type="text" name="tag" value="viewteams" style="display:none" />
 	<input type="text" name="search_string"  value="<?php echo $search_string?>" class="vform"/>
 	<input type="submit" value="Filter" class="button" />
 </form>	
@@ -26,6 +28,10 @@
 	</thead>
 	<thead>
 		<?php
+			$qry_team_SQL = $qry_team_SQL . " WHERE teamname Like '%".$search_string."%' 
+						OR teamlead Like '%".$search_string."%'
+						OR status Like '%".$search_string."%' 
+						";
 			$team_query = mysql_query( $qry_team_SQL );
 			for( $incr = 0; $incr< mysql_num_rows($team_query); $incr++) {
 				$row = mysql_fetch_array( $team_query );
