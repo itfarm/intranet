53be9ec5efa67155ba768aaaf77b2f452183d560
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-07-04 02:38:23 +0300

    Fixed sumbission bugs for forms variable names

--- modules/tasks/profile/location.php
+++ modules/tasks/profile/location.php
@@ -3,6 +3,26 @@
 	$dbcnx = db_connection($db_host,$db_user,$db_password) or die( mysql_error() );
 	$db_select = db_select($db_name,$dbcnx) or die( mysql_error() );
 	$id= $_SESSION['id'];
+	$form_changed = "false";
+	// Get POST Information
+	$form_changed = $_POST['changed'];
+	if( $form_changed == "true" ) {
+		$location = $_POST['location'];
+		$address = $_POST['address'];
+		$city = $_POST['city'];
+		$zip = $_POST['zip'];
+		$country = $_POST['country'];
+		$id = $_POST['id'];
+		$query_string = "UPDATE authuser SET " . "location = '" . $location . "', ".
+						"address = '" . $address . "', " .
+						"city = '" . $city . "', " .
+						"zip = '". $zip . "', " .
+						"country = '". $country . "'" .
+						" WHERE id = '" . $id . "';";
+		$location_query = mysql_query($query_string) or die( mysql_error() );
+		$form_changed = "false";
+		echo "<p>Current location updated</p>";
+	}
 ?>
 <form method="post" action="<?php $_SERVER['PHP_SELF']?>">
 	<?php
@@ -12,7 +32,7 @@
 	<table>
 		<tr>
 			<td><label>Location</label></td>
-			<td><input type="text" name="username" value="<?php echo $row['location']?>"></td>
+			<td><input type="text" name="location" value="<?php echo $row['location']?>"></td>
 		</tr>
 		<tr>
 			<td><label>Address</label></td>
@@ -30,6 +50,7 @@
 			<td><label>Country</label></td>
 			<td><input type="text" name="country" value="<?php echo $row['country']?>"></td>
 			<input type="hidden" name="id" value="<?php echo $id ?>">
+			<input type="hidden" name="changed" value="true">
 		</tr>
 		<tr>
 			<td> <input type="submit" name="change" value="Submit" class="button"> </td>
