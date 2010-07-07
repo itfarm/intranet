1e9cf457f10d1a732627530b120e31e909fcbc99
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-07-07 04:29:03 +0300

    Changed include functions line to functionsspecial

--- modules/tasks/dashboard/viewdocuments.php
+++ modules/tasks/dashboard/viewdocuments.php
@@ -4,7 +4,7 @@
 	@include_once('config_imported/coresql.inc.php');
 	@include_once('config_imported/functionsdate.inc.php');
 	@include_once('config_imported/functionsgeneral.inc.php');
-	@include_once('config_imported/functions.inc.php');
+	@include_once('config_imported/functionsspecial.inc.php');
 	// initialize host and database
     @include_once('config_imported/settings.inc.php');
 	
