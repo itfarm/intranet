51f9d4e8fae5f04678f404e3e96d3a24bd38483f
John Francis Mukulu <john.f.mukulu@gmail.com>
2010-07-27 05:09:20 +0300

    Improved interface of survey app Incorported help into the systemy

---
+++ modules/polls/help.php
@@ -0,0 +1,92 @@
+<?php echo '
+      <p><b><u>HOW TO CREATE SURVEY</u></b></p>
+      <p>With the right sidebar you can create a new survey, add questions and
+		answers and of course edit approximately anything. Here is the meaning
+		of the icons you\'ll see in the admin interface (I\'ll use the term &quot;
+		item &quot; to generically designate a survey,
+        a question or an answer - the current item is the item that is on the
+        line where you clicked the icon):</p>
+      <table border="0" width="100%">
+        <tr>
+          <td width="33%" valign="top" bgcolor="#666666">
+            <div align="center"><b><font color="#FFFFFF">icon</font></b></div>
+          </td>
+          <td bgcolor="#666666">
+            <div align="center"><b><font color="#FFFFFF">action</font></b></div>
+          </td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/add.gif" width="16" height="16"></div>
+          </td>
+          <td>add an item in the database. before clicking edit the new item on
+            the left of this icon</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/alter.gif" width="16" height="16"></div>
+          </td>
+          <td>validate the changes you have made to the current item</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/delete.gif" width="16" height="16"></div>
+          </td>
+          <td>delete an item. <b>no confirmation asked!!</b></td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/insert.gif" width="16" height="16"></div>
+          </td>
+          <td>insert an empty item before the current one</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/up.gif" width="16" height="16"></div>
+          </td>
+          <td>move the current item up in the current list</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/down.gif" width="16" height="16"></div>
+          </td>
+          <td>move the current item down in the current list</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/inspect.gif" width="16" height="16"></div>
+          </td>
+          <td>go one level below: in survey view, this allows you to edit the
+            questions of the current survey ; in question view this lets you edit
+            the answers to the current question</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/reset.gif" width="16" height="16"></div>
+          </td>
+          <td>this lets you reset the results of the current survey</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/result.gif" width="16" height="16"></div>
+          </td>
+          <td>this lets you see the results of the current survey</td>
+        </tr>
+        <tr valign="top">
+          <td width="33%" align="center" valign="middle">
+            <div align="center"><img src="images/history.gif" width="16" height="16"></div>
+          </td>
+          <td>this lets you see the history of answers made to the current survey</td>
+        </tr>
+      </table>
+      <p>You have several options for a survey:</p>
+      <ul>
+      <li><b>Single vote:</b> Allows or disallows a same person to vote multiple times to the survey. Check can be done using cookies or based on the IP address.<br>&nbsp;</li>
+      <li><b>Log:</b> Enables the save of <b>all</b> the answers made to the survey. You\'ll be able to check exactly what each voter answered to each question.<br>&nbsp;</li>
+      <li><b>Required:</b> If this is checked, then a voter must answer to all the questions of the survey.<br>&nbsp;</li>
+      <li><b>Closed:</b> No more votes can be taken for that survey.<br>&nbsp;</li>
+      </ul>
+      <p>With the above information you should be able to create a survey questionaire.</p>
+      ';
+?>
+
--- modules/polls/templates/classic/footer.tpl
+++ modules/polls/templates/classic/footer.tpl
@@ -5,17 +5,6 @@
 * (c) nabocorp, 2002 *
 **********************
 
-Here are the tags valid here:
 
-{SURVEY_ID}: id of the current survey
-{SURVEY_TITLE}: title of the current survey
-{SURVEY_URL}: url of the current survey (as defined in the database)
-{NABOPOLL_LOGO}: powered by nabopoll logo
-
--->
-
-{NABOPOLL_LOGO}
 </td></tr></table>
 </font>
-</body>
-</html>
